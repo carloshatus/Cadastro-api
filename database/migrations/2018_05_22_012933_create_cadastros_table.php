338b8659bad6da9b19cb609e4370957bac500454
carloshatus <carlos.hatus@hotmail.com>
2018-05-22 15:20:20 -0300

    migration

--- database/migrations/2018_05_22_012933_create_cadastros_table.php
+++ database/migrations/2018_05_22_012933_create_cadastros_table.php
@@ -16,18 +16,18 @@ class CreateCadastrosTable extends Migration
         Schema::create('cadastros', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome');
-            $table->string('email');
-            $table->date('data_nascimento');
+            $table->string('email')->nullable();
+            $table->date('data_nascimento')->nullable();
             $table->string('estado_civil')->nullable();
-            $table->string('endereco');
-            $table->string('bairro');
-            $table->string('cidade');
-            $table->string('cep');
-            $table->string('telefone');
+            $table->string('endereco')->nullable();
+            $table->string('bairro')->nullable();
+            $table->string('cidade')->nullable();
+            $table->string('cep')->nullable();
+            $table->string('telefone')->nullable();
             $table->string('nome_mae')->nullable();
             $table->string('nome_pai')->nullable();
-            $table->string('cpf');
-            $table->string('rg');
+            $table->string('cpf')->nullable();
+            $table->string('rg')->nullable();
             $table->string('titulo')->nullable();
             $table->timestamps();
         });
